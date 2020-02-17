ce051b9dfd5471e73cdba2bf6f9d427ed9b82cc5
Fanny Huang <>
2020-02-17 14:45:49 +0100

    Add functions to parse a chemical Formula: -parseChemmicalFormula(), getClosingBacketIndex(), getNumberOfMolecules(), getElement()

--- src/chemicalFormula.js
+++ src/chemicalFormula.js
@@ -1,3 +1,78 @@
+let brackets = {
+    '[': ']',
+    '(': ')',
+    '{': '}',
+};
+
+function isElement(str) {
+    return str.match(/[A-Z]/);
+}
+
+export function isBracket(c) {
+    return c === '[' || c === '(' || c === '{';
+}
+
+export function getClosingBracketIndex(str) {
+    if (!str)
+        return null;
+    let openingBracket = str[0];
+    let closingBracket = brackets[str[0]];
+    var depth = 0;
+    var i = 0;
+    while (str[i]) {
+        if (str[i] === openingBracket)
+            depth++;
+        else if (str[i] === closingBracket)
+            depth--;
+        if (depth === 0)
+            return i;
+        i++;
+    }
+    return -1;
+}
+
+export function getElement(str) {
+    var number = 1;
+    var element = '';
+    if (str.length == 1)
+        element = str[0];
+    else {
+        let isTwoLetters = str[1].match(/a-z/);
+        element = str.substr(0, isTwoLetters ? 2 : 1);
+        number = getNumberOfMolecules(str, isTwoLetters ? 2 : 1);
+    }
+    return { [element]: number };
+}
+
+export function getNumberOfMolecules(str, index) {
+    let ret = parseInt(str.substr(index), 10);
+    return isNaN(ret) ? 1 : ret;
+}
+
+
+function parseElement(input, multiplyBy = []) {
+    var ret = {};
+    var i = 0;
+    var indexBracket = 0;
+
+    // while (input[i]) {
+    //     if (isBracket(input[i])) {
+    //         indexBracket = getClosingBracketIndex(input) + i;
+    //         numberOfMolecules = getNumberOfMolecules(input, indexBracket + 1);
+    //         ret = parseElement(input.substr(i, indexBracket - i), [...multiplyBy, numberOfMolecules]);
+    //         i = indexBracket;
+    //     }
+    //     else if (isElement(input[i])) {
+    //         element = input.substr(i, input[i + 1].match(/[a-z]/) ? 1 : 2);
+    //         console.log(element);
+
+    //     }
+    //     i++;
+    // }
+    return ret
+}
+
+
 
 /**
  * 
@@ -11,6 +86,9 @@ export function parseChemicalFormula(input) {
 
     // isFormulaValid()
     ret = {};
+
+    console.log(parseElement(input));
+
     return null;
     
 }
--- src/chemicalFormula.test.js
+++ src/chemicalFormula.test.js
@@ -1,10 +1,66 @@
-import { parseChemicalFormula } from './chemicalFormula'
+import { parseChemicalFormula, getClosingBracketIndex, getNumberOfMolecules, getElement } from './chemicalFormula'
+
+describe("Get the index of the closing bracket", () => {
+    test("Simple test", () => {
+        const input = "(test)"
+        expect(getClosingBracketIndex(input)).toEqual(5);
+    });
+    
+    test("Nested test", () => {
+        const input = "( [ 2 ])"
+        expect(getClosingBracketIndex(input)).toEqual(7);
+        expect(getClosingBracketIndex(input.substr(2))).toEqual(6 - 2);
+    });
+
+    test("Nested with same brackets", () => {
+        const input = "[[ 2 ]]"
+        expect(getClosingBracketIndex(input)).toEqual(6);
+        expect(getClosingBracketIndex(input.substr(1))).toEqual(5 - 1);
+    });
+    
+})
+
+
+describe("Get number given a string index", () => {
+    test("Single number", () => {
+        const input = "(test)2"
+        expect(getNumberOfMolecules(input, 6)).toEqual(2);
+    });
+    test("No number", () => {
+        const input = "(test)"
+        expect(getNumberOfMolecules(input, 5)).toEqual(1);
+    });
+    test("Two digits number", () => {
+        const input = "(test)12"
+        expect(getNumberOfMolecules(input, 6)).toEqual(12);
+    });
+
+});
+
+describe("Get Element in a string", () => {
+    test("Simple", () => {
+        expect(getElement("H2O")).toEqual({H: 2});
+    })
+    test("Simple", () => {
+        expect(getElement("O")).toEqual({O: 1});
+    })
+    test("Two digits elements", () => {
+        expect(getElement("O12")).toEqual({O: 12});
+    })
+    test("Tricky", () => {
+        expect(getElement("HO")).toEqual({H: 1});
+    })
+});
+
 
 describe("Parse a Chemical Formula function", () => {
     test("Empty string", () => {
         const inputs = ["", "            ", " \t\t   "];
         inputs.forEach(input => {
-            expect(parseChemicalFormula(input)).toEqual(null)
+            expect(parseChemicalFormula(input)).toEqual(null);
         })
+    })
+    test("H20", () => {
+        expect(parseChemicalFormula("H2O").toEqual({H: 2, O: 1}));
     })
 })
